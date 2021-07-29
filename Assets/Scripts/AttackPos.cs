bf3340b705baa6b8b61ded28d9cf27bf1adf9f66
hyojin-ahn <angela991014@naver.com>
2021-07-29 14:36:18 +0900

    Revert "targetready"
    This reverts commit d3616ae2dd2306e58dc3aea8c9922ac04b7386fd.

--- Assets/Scripts/AttackPos.cs
+++ Assets/Scripts/AttackPos.cs
@@ -7,22 +7,20 @@ public class AttackPos : MonoBehaviour
     public GameObject bulletFactory;  //ÃÑ¾Ë °øÀå
 
     float currentTime;
-    bool targetready;
 
     public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        targetready= GameObject.Find("Player");
+
     }
 
     // Update is called once per frame
-    
     void Update()
     {
         currentTime += Time.deltaTime;
 
-        if(currentTime > 1  && targetready==true)
+        if(currentTime > 1)
         {
             GameObject bullet = Instantiate(bulletFactory);
             bullet.transform.position = transform.position;
