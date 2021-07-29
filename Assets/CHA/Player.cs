4d595520535d055cad5b9f082bfcf6e4b29dba6d
ckwngp <88120966+ckwngp@users.noreply.github.com>
2021-07-29 16:54:54 +0900

    player
    playermove 추가

---
+++ Assets/CHA/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int playerHp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHp = 1000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
