9047f89764bfaeb80b06a10871d886c80f963020
hyojin-ahn <angela991014@naver.com>
2021-07-30 15:08:43 +0900

    7030_1508

--- Assets/HYO/HYO_Scripts/BulletMove.cs
+++ Assets/HYO/HYO_Scripts/BulletMove.cs
@@ -26,7 +26,19 @@ public class BulletMove : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        Destroy(this.gameObject);
+        //Debug.Log(collision.gameObject.name);
+
+        //충돌한 반대 방향으로 튕기기
+        //방향과 힘을 갖는 Vector3
+        Vector3 direction = transform.position - collision.gameObject.transform.position;
+
+        //direction = direction * 1000;
+        //normalized 를 함으로 벡터의 방향은 그대로 두고 크기만 1.0로 맞춰 주는 것
+        direction = direction.normalized * 1000;
+
+        //가져온 힘을 볼 오브젝트에 다시 전해 줌.
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(direction);
+
     }
 
 }
