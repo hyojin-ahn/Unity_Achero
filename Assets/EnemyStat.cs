a78a29c547a2f8fb34b4fb3b15fe35ad84e32a27
김희진 <67904074+Rrrriaa@users.noreply.github.com>
2021-08-05 16:12:11 +0900

    0805
    enemy prefeb

---
+++ Assets/EnemyStat.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStat : MonoBehaviour
+{
+    public int hp;
+    public int power;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/KHJ/KHJ_Scripts/EnemyStat.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStat : MonoBehaviour
+{
+
+    public int hp;
+    public int power;
+
+
+
+    private void Start()
+    {
+        
+    }
+
+    public void damaged(int damage)
+    {
+        hp -= damage;
+    }
+
+}
