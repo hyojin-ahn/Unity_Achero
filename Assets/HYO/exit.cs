2b0c2b6758be096b3dadaa73dda3a7326d99c148
hyojin-ahn <angela991014@naver.com>
2021-08-13 17:01:25 +0900

    0813_gengar

---
+++ Assets/HYO/exit.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exit : MonoBehaviour
+{
+    public void OnClickExit()
+    {
+        Application.Quit();
+        Debug.Log("Button Click");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
